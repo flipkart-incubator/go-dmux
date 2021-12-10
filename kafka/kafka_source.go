15ed5a4c740d1b5815e6bb0a001683c8967dd667
rashiChandola <61137511+rashiChandola@users.noreply.github.com>
2021-12-10 10:09:46 +0530

    logger

--- kafka/kafka_source.go
+++ kafka/kafka_source.go
@@ -3,6 +3,7 @@ package kafka
 import (
 	"time"
     "os"
+    "log"
 
 	"github.com/Shopify/sarama"
 	"github.com/go-dmux/kafka/kazoo-go"
@@ -81,10 +82,15 @@ func (k *KafkaSource) Generate(out chan<- interface{}) {
 		config.Offsets.Initial = sarama.OffsetNewest
 	}
 
+    log.Println("now checking if sasl enabed or not ")
+
 	if kconf.SASLEnabled {
+	        log.Println("sasl is enabled")
     		//sarama config plain by default
     		config.Net.SASL.User = kconf.SASLUsername
+    		log.Println("usernname is ", config.Net.SASL.User)
     		config.Net.SASL.Password = os.Getenv("KAFKA_CLIENT_USER_PASSWORD")
+    		log.Println("password is ", config.Net.SASL.Password)
     		config.Net.SASL.Enable = true
     	}
 
